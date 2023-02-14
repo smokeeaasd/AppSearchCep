a4fbaa25e219112124386c6f29fc0d96156ffd65
Usuario <Usuario@CPD03-M10>
2023-02-14 14:32:19 -0300

    Adicionar arquivos de projeto.

---
+++ AppSearchCep/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AppSearchCep.View;
+
+namespace AppSearchCep
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Inicial();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppSearchCep/Model/Bairro.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppSearchCep.Model
+{
+    public class Bairro
+    {
+    }
+}
