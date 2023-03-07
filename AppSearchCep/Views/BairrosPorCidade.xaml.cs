546b8661eef2ddcbc77bff72af0458109fda83c1
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-03-07 15:57:55 -0300

    views

---
+++ AppSearchCep/Views/BairrosPorCidade.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppSearchCep.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BairrosPorCidade : ContentPage
+    {
+        public BairrosPorCidade()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}
