852bdcdcb8d6f40e7b5c30bb9dd040766511728b
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-03-21 15:09:43 -0300

    Adicionando controles personalizados

---
+++ AppSearchCep/Controls/PageButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppSearchCep.Controls
+{
+    public class PageButton : Button
+    {
+        public Page TargetPage { get; set; }
+
+        public PageButton()
+        {
+            Clicked += OnClicked;
+        }
+
+        private void OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync((Page)Activator.CreateInstance(TargetPage.GetType()));
+        }
+    }
+}
