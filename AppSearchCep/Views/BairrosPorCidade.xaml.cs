c6cad73c7e35ff87bbb219d694fed5c98598c880
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-03-21 15:10:14 -0300

    Editando Views

--- AppSearchCep/Views/BairrosPorCidade.xaml.cs
+++ AppSearchCep/Views/BairrosPorCidade.xaml.cs
@@ -2,6 +2,7 @@
 using AppSearchCep.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -15,15 +16,55 @@ namespace AppSearchCep.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BairrosPorCidade : ContentPage
     {
+        ObservableCollection<Cidade> lista_cidades = new ObservableCollection<Cidade>();
+        ObservableCollection<Bairro> lista_bairros = new ObservableCollection<Bairro>();
         public BairrosPorCidade()
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
+
+            pck_cidade.ItemsSource = lista_cidades;
+            lst_bairros.ItemsSource = lista_bairros;
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void pck_cidade_SelectedIndexChanged(object sender, EventArgs e)
         {
-            List<Cidade> arr_cidades = await DataService.GetCidadesByUF(pck_uf.SelectedItem.ToString());
+            try
+            {
+                Picker disparador = sender as Picker;
+
+                Cidade cidade_selecionada = disparador.SelectedItem as Cidade;
+
+                List<Bairro> arr_bairros = await DataService.GetBairrosByIdCidade(cidade_selecionada.id_cidade);
+
+                lista_bairros.Clear();
+
+                arr_bairros.ForEach((i) => lista_bairros.Add(i));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void pck_uf_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Picker disparador = sender as Picker;
+
+                string estado_selecionado = disparador.SelectedItem as string;
+
+                List<Cidade> arr_cidades = await DataService.GetCidadesByUF(estado_selecionado);
+
+                lista_cidades.Clear();
+
+                arr_cidades.ForEach((i) => lista_cidades.Add(i));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 }
--- AppSearchCep/Views/Menu.xaml.cs
+++ AppSearchCep/Views/Menu.xaml.cs
@@ -16,25 +16,5 @@ namespace AppSearchCep.Views
         {
             InitializeComponent();
         }
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new BuscaCepPorLogradouro());
-        }
-
-        private void Button_Clicked_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Button_Clicked_2(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new BairrosPorCidade());
-        }
-
-        private void Button_Clicked_3(object sender, EventArgs e)
-        {
-
-        }
     }
 }
